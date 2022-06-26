88a3334b7c05b3e6abded85fd64939d83df7e405
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-06-26 18:17:57 +0300

    creating contacts table and styling it

--- src/pages/DisplayContacts.js
+++ src/pages/DisplayContacts.js
@@ -1,7 +1,45 @@
 import React from "react";
+import { AiOutlineEdit, AiOutlineDelete } from "react-icons/ai";
 
 const DisplayContacts = () => {
-  return <div>DisplayContacts</div>;
+  return (
+    <div>
+      <div className="title-table flex-cont">
+        <h2>Contacts</h2>
+        <button className="add-btn">Add Contact</button>
+      </div>
+
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Full Name</th>
+              <th>Phone Number</th>
+              <th>Relationship Status</th>
+              <th>Email</th>
+              <th>Location</th>
+              {/* <th></th> */}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Stephanelle Rachwan</td>
+              <td>76871120</td>
+              <td>Divorced</td>
+              <td>stephanellerachwan@gmail.com</td>
+              <td>Planet Mars</td>
+              <td>
+                <AiOutlineDelete />
+              </td>
+              <td>
+                <AiOutlineEdit />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default DisplayContacts;
