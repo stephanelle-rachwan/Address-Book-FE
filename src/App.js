5277f57fac9001b59a2c6f6be6f2d819796f7d3f
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-06-26 10:47:03 +0300

    creating the routes and pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LogIn from "./pages/LogIn";
+import Register from "./pages/Register";
+import EditContacts from "./pages/EditContacts";
+import DisplayContacts from "./pages/DisplayContacts";
+import AddContacts from "./pages/AddContacts";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="login" element={<LogIn />}></Route>
+        <Route path="register" element={<Register />}></Route>
+        <Route path="contacts/edit" element={<EditContacts />}></Route>
+        <Route path="contacts" element={<DisplayContacts />}></Route>
+        <Route path="contacts/add" element={<AddContacts />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/DisplayContacts.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DisplayContacts = () => {
+  return <div>DisplayContacts</div>;
+};
+
+export default DisplayContacts;
