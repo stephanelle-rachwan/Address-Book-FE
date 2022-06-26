9972445b4f27d5b9062821fbfe695984953b0813
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-06-26 15:16:23 +0300

    modifying navbar and styling it

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,21 @@
 import React from "react";
-import { NavLink, useLocation } from "react-router-dom";
 
 const Navbar = () => {
-  const location = useLocation();
-  // an array of objects
-  const navItems = [
-    { link: "/contact", name: "Contact" },
-    { link: "/register", name: "Register" },
-    { link: "/login", name: "Log In" },
-    { link: "/", name: "Log Out" },
-  ];
-  console.log(location);
   return (
-    <nav className="top-bar">
-      <ul className="nav-items">
-        {navItems.map((navItem, index) => {
-          return (
-            <NavLink key={index} to={navItem.link}>
-              <li>{navItem.name}</li>
-            </NavLink>
-          );
-        })}
-        {location.pathname.includes("contact") && (
-          <NavLink to="/contacts/add">
-            <li>Add Contact</li>
-          </NavLink>
-        )}
-      </ul>
+    <nav>
+      <div className="logo">
+        <a href="contacts">THE Address Book</a>
+      </div>
+
+      <div className="nav-list">
+        <ul>
+          <li>
+            <a href="register"> Register</a>
+            <a href="login"> Log In</a>
+            <a href="logout"> Log Out</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
