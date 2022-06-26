0416a01be8dd28755536cbb48055803bd1d0f2d2
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-06-27 01:35:02 +0300

    linking the login feature

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const LogIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  async function login() {
+    try {
+      const res = await fetch("http://localhost:3000/api/user/login", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ email: email, password: password }),
+      });
+      const data = await res.json();
+      console.log(data);
+      window.location.href = "/contacts";
+    } catch (error) {
+      alert("wrong data");
+    }
+  }
+
   return (
     <div className="login">
       <h1> Log In </h1>
       <form className="login-form">
         <div className="input-box">
           <input
+            onChange={(e) => {
+              setEmail(e.currentTarget.value);
+              console.log(email);
+            }}
             type="email"
             placeholder="Email address*"
             name="email"
@@ -15,13 +38,17 @@ const LogIn = () => {
         </div>
         <div className="input-box">
           <input
+            onChange={(e) => {
+              setPassword(e.currentTarget.value);
+              console.log(password);
+            }}
             type="password"
             placeholder="Password*"
             name="password"
             required
           ></input>
         </div>
-        <button type="button" className="login-btn">
+        <button type="button" className="login-btn" onClick={(e) => login()}>
           Log In
         </button>
       </form>
